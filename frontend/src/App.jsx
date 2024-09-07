d8b37f7d52541ea8e49b42000e897750f0cac393
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-09-07 15:31:44 +0000

    update feature sourie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Header from './components/Header.jsx';
 import About from './components/About.jsx';
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color='0, 0, 0'
+        outerAlpha={0.2}
+        innerScale={1.7}
+        outerScale={2}
+        trailingSpeed={10}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
       <Navbar/>
       <Header/>
       <About />
--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -5,7 +5,7 @@ let About = () => {
         <div className="py-5 bg-light">
             <div className="row gx-5 justify-content-center">
                 <div className="col-xxl-8">
-                    <div className="text-center my-5">
+                    <div className="text-center my-3">
                         <h2 className="display-5 fw-bolder">
                             <span>À propos</span>
                         </h2>
@@ -16,10 +16,10 @@ let About = () => {
                             Je suis étudiant en Data Sciences et Société du Numérique, passionné par l’utilisation des données comme levier de transformation. Mon portfolio "Data for Social Empowerment" illustre ma vision : comment les données peuvent non seulement critiquer le statu quo mais aussi offrir des solutions innovantes aux défis sociaux et techniques. Explorez mes projets, allant des initiatives pour l’équité sociale aux travaux techniques issus de cours et recherches personnelles, pour découvrir comment j’allie compétences analytiques et créativité.
                         </p>
                         <div className="d-flex justify-content-center fs-2 gap-4">
-                            <a className="text-black" href="#!">
+                            <a className="text-black" href="https://www.linkedin.com/in/mandresy-andri/" target='_blank'>
                                 <i className="bi bi-linkedin"></i>
                             </a>
-                            <a className="text-black" href="#!">
+                            <a className="text-black" href="https://github.com/mandresyandri" target='_blank'>
                                 <i className="bi bi-github"></i>
                             </a>
                         </div>
