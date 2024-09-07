f81a4f4bd0f469fd13dc9a8ba97ff232d19ead71
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-09-07 12:36:23 +0000

    update graphism navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,21 +3,21 @@ let NavBar = () => {
         <nav className="navbar navbar-expand-lg nav-bg bg-white py-3">
             <div className="container px-5">
                 <a className="navbar-brand" href="#">
-                    <span>Mandresy Andri</span>
+                    <span className="fw-bolder">Mandresy Andri</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bold">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bold">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Projects</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
                     <a className="btn btn-outline-dark ms-md-2" href="http://www.mandresyandri.fr/">
