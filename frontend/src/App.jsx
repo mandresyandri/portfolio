4fb958d6aca96af1965b0feaefb62b7ba086caca
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-09-07 15:17:51 +0000

    update ticket about

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import Header from './components/Header.jsx';
+import About from './components/About.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <About />
     </div>
   );
 }
